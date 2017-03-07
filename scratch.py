d44545559a225d5167995df30fcf3642f9e7b13d
xianny <xianny@gmail.com>
2017-03-07 14:51:29 -0500

    seed scripts

--- scratch.py
+++ scratch.py
@@ -1,11 +1,19 @@
 import transitfeed
-
+import mta
 
 
 print "hello"
 feed_filename = './data/google_transit.zip'
 schedule = transitfeed.Schedule()
 schedule.Load(feed_filename)
-print schedule.GetAgencyList().encode('utf-8')
-print "ok"
-    
+
+all_edges = mta.get_edges_with_schedule(schedule)
+
+stops_and_edges = {}
+
+for stop in schedule.stops.values():
+    stops_and_edges[stop.stop_id] = mta.get_edges_for_stop(all_edges, stop)
+
+
+print len(stops_and_edges)
+print len(schedule.stops.values())
